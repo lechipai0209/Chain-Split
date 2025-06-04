60399c4fd1e4be4cc40a1d6ed7d633105f3abc23
kimery <king0209king0209@gmail.com>
2025-06-04 17:58:12 +0800

    feat: 儲存空間優化

--- contract/programs/contract/src/state.rs
+++ contract/programs/contract/src/state.rs
@@ -1,43 +1,33 @@
-#[account]
-pub strcut GroupAccount{
+#[account]  // 357
+pub strcut GroupAccount{ 
     pub payer: Pubkey,
-    pub name: [u8, 64],
-    pub member_count: u8, // 最多限制50位參加者
-    pub expense_count: u64,
+    pub name: [u8, 32],
+    pub expense_count: u16, //一般版 66筆最高
     pub currency: Currency,
     pub onchain_payment_enabled: bool,
-    pub balance_check_enabled: bool,
+    pub member: [Pubkey; 8],
+    pub net: [u32; 8],
+    pub expense: [Pubkey; 1], //一般版 66筆最高
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
-pub enum CurrencyType {
+pub enum Currency {
     TWD,
     USD,
 }
 
 
-#[account]
-pub struct MemberAccount {
-    pub group: Pubkey,
-    pub payer : Pubkey,
-    pub pre_paid: u64,
-    pub net: i64,
-}
 
-#[account]
+#[account] // 107+8++32+8=150
 pub struct ExpenseAccount {
-    pub group: Pubkey,
+    pub group: Pubkey,  
     pub payer: Pubkey,
     pub amount: u64,
     pub name: [u8; 32],
-    pub description: [u8; 64],
+    pub participants: [u8; 8],
+    pub expense: [u32; 10],
+    pub signed: [u8; 10],
     pub is_reversed: bool,
 }
 
-#[account]
-pub struct ExpenseParticient {
-    pub expense_account: Pubkey,
-    pub participant: Pubkey,
-    pub payment: u64,
-    pub signed: bool,
-}
+
