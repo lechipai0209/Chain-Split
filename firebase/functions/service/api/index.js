f66f4c00e13e3c78fed14b994fec566b087b1000
kimery <king0209king0209@gmail.com>
2025-06-27 01:34:16 +0800

    feat: backend api

---
+++ firebase/functions/service/api/index.js
@@ -0,0 +1,12 @@
+// 1. the all group this address belongs to(just name host time)
+// 2. a single group's details(all including records)
+// 3. self user data
+// 4. host group 
+// 5. all unclosed account
+// 6. open tx details(by tx)
+// 7. open account details(by account)
+// 8. find groups by name
+// 9. find account by name
+// 10. find group net table
+
+
