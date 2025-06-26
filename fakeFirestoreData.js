b8e76ed9013c12fa22db3058392e22580ebb1429
kimery <king0209king0209@gmail.com>
2025-06-26 18:00:32 +0800

    doc: fake firestore data

---
+++ fakeFirestoreData.js
@@ -0,0 +1,17 @@
+user: {
+    "0x0000000000000000000000000000000000" : {
+        "name": "name",
+        "groups": [
+            "0x0000000000000000000000000000000000"
+        ],
+        deviceTokens: [
+            "5gbjo43fvheyg54",
+            "eptgj45bgryjogi4",
+            "gjeoyjg45jljrhrj",
+        ],
+    }
+}
+
+group: {
+    ""
+}
