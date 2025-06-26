d7872fbe2a20541ebac8c54e683b90184069dbb5
kimery <king0209king0209@gmail.com>
2025-06-27 01:34:06 +0800

    feat: fake data

--- fakeFirestoreData.js
+++ fakeFirestoreData.js
@@ -1,17 +1,43 @@
 user: {
-    "0x0000000000000000000000000000000000" : {
+    "0x00htrhtrjukyukoipuj7tvjepyjrrbhtrjhr" : {
         "name": "name",
         "groups": [
-            "0x0000000000000000000000000000000000"
+            "0x00hr76j7tjrjhrmjdahnhuk5j6isfwfw"
         ],
-        deviceTokens: [
-            "5gbjo43fvheyg54",
+        "deviceTokens": [
+            "5gbjo43fvheyg54g",
             "eptgj45bgryjogi4",
             "gjeoyjg45jljrhrj",
         ],
     }
-}
+} ;
 
 group: {
-    ""
+    "groupName": "Japan Travel",
+    "group": "0x00hr76j7tjrjhrmjdahnhuk5j6isfwfw",
+    "members": ["0x00htrhtrjukyukoipuj7tvjepyjrrbhtrjhr"],
+    "host": "0x00htrhtrjukyukoipuj7tvjepyjrrbhtrjhr",
+    "createdTime": "80000342324",
+    "closed": false,
+    "index": 0,
+    "records": [
+        // create
+        {
+            "event": "ExpenseCreatedEvent",
+            "txSig": "gejgtfonf8dchc85vmxnvzbl7jht",
+            "group": "0x00hr76j7tjrjhrmjdahnhuk5j6isfwfw",
+            "signer": "0x00htrhtrjukyukoipuj7tvjepyjrrbhtrjhr",
+            "account": "0x00hr76j7tjrjhrmjdahnhuk5j6isfwfw",
+            "groupName": "Japan Travel",
+            "time": "2020-01-01",
+            "index": 0
+        },
+        {
+            ...
+        },
+        {
+            ...
+        }
+    ]
 }
+
